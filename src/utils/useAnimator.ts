46ef0dbf6695c978b5fd18c404c8c4d5c9d3c3e1
Reyshawn <reshawnchang@gmail.com>
2023-06-07 12:02:01 +0800

    Add the `cubicBezier` function;

--- src/utils/useAnimator.ts
+++ src/utils/useAnimator.ts
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from "react"
-
+import { noop } from "rxjs"
+import { cubicBezier } from "./cubicBezier"
 
 export enum AnimationTimingCurve {
   linear,
@@ -7,6 +8,21 @@ export enum AnimationTimingCurve {
   easeOut,
   easeInOut
 }
+
+
+const getCurve = (type: AnimationTimingCurve) => {
+  switch (type) {
+    case AnimationTimingCurve.linear:
+      return (n: number) => n
+    case AnimationTimingCurve.easeIn:
+      return cubicBezier(0.42, 0, 1, 1)
+    case AnimationTimingCurve.easeOut:
+      return cubicBezier(0, 0, 0.58, 1)
+    case AnimationTimingCurve.easeInOut:
+      return cubicBezier(0, 0, 0.58, 1)
+  }
+}
+
 
 interface AnimatorConfiguration {
   duration: number
@@ -21,6 +37,12 @@ interface AnimationKeyframe {
   current: () => number[]
 }
 
+interface AnimationStatus {
+  startTime: number
+  pausedProgress: number
+  rafId: number | null
+}
+
 
 interface Animator {
   start: () => void,
@@ -30,27 +52,34 @@ interface Animator {
 }
 
 
-let startTime = 0
-let pausedProgress = 0
-let rafId: number | null = null
+// let startTime = 0
+// let pausedProgress = 0
+// let rafId: number | null = null
 
 
 export function useAnimator<T extends HTMLElement>(config: AnimatorConfiguration): [AnimationKeyframe, Animator] {
-  const { duration } = config
+  const { duration, easing } = config
   const [rendering, setRendering] = useState(0)
   const keyframe = useRef<AnimationKeyframe>({
     status: "inactive",
     progress: 0,
     current() {
-      const p = this.progress
+      const curve = getCurve(easing)
+      const p = curve(this.progress)
 
       return config.from.map((fromValue, index) => fromValue + (config.to[index]  - fromValue) * p)
     }
   })
 
+  const status = useRef<AnimationStatus>({
+    startTime: 0,
+    pausedProgress: 0,
+    rafId: null
+  })
   
   const tick = useCallback((now: DOMHighResTimeStamp) => {
     const kf = keyframe.current
+    const {startTime, pausedProgress} = status.current
     if (kf.progress >= 1) {
       kf.status = "finished"
       kf.progress = 1
@@ -62,53 +91,58 @@ export function useAnimator<T extends HTMLElement>(config: AnimatorConfiguration
 
     kf.progress =  pausedProgress + elapsed / duration
     setRendering(i => i+1)
-    rafId = requestAnimationFrame(tick)
+    status.current.rafId = requestAnimationFrame(tick)
   }, [])
 
   const animator = useMemo<Animator>(() => ({
 
     start() { 
-      if (rafId) {
-        cancelAnimationFrame(rafId)
-        rafId = null
+      const s = status.current
+      if (s.rafId) {
+        cancelAnimationFrame(s.rafId)
+        status.current.rafId = null
       }
 
 
       const kf = keyframe.current
       kf.status = "running"
       kf.progress = 0
-      pausedProgress = 0
+      s.pausedProgress = 0
 
-      startTime = performance.now()
-      tick(startTime)
+      s.startTime = performance.now()
+      tick(s.startTime)
     },
 
     pause() {
-      if (rafId) {
-        cancelAnimationFrame(rafId)
-        rafId = null
-        pausedProgress = keyframe.current.progress
+      const s = status.current
+      if (s.rafId) {
+        cancelAnimationFrame(s.rafId)
+        s.rafId = null
+        s.pausedProgress = keyframe.current.progress
         keyframe.current.status = "pasued"
         setRendering(i => i+1)
       }
     },
 
     set(progress: number) {
+      const s = status.current
       if (keyframe.current.status === "running") {
         return
       }
 
       keyframe.current.status === "inactive"
       keyframe.current.progress = progress
+      s.pausedProgress = progress
       setRendering(i => i+1)
     },
 
     continue() {
+      const s = status.current
       const kf = keyframe.current
       kf.status = "running"
-      startTime = performance.now()
+      s.startTime = performance.now()
       
-      tick(startTime)
+      tick(s.startTime)
     }
 
   }), [])
