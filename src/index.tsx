e15072989af95947fc5dc649becf29a2babfc9f0
Reyshawn <reshawnchang@gmail.com>
2023-08-06 20:37:29 +0800

    Use the `createRoot`;

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,16 +10,18 @@ import { Provider } from 'react-redux';
 
 import { store } from './store';
 
-ReactDOM.render(
+
+
+const root = createRoot(document.getElementById('root')!)
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
         <App />
       </Router>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
