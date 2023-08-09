0547dec0a93bd2b4b9bc3828f690005405dddaec
Reyshawn <reshawnchang@gmail.com>
2023-08-09 23:33:00 +0800

    Refactor the `useDisclosure` hook.

--- src/utils/useDisclosure.ts
+++ src/utils/useDisclosure.ts
@@ -1,32 +1,35 @@
 import { useFloating, UseFloatingOptions } from "@floating-ui/react-dom";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import useClickAway from "./useClickAway";
 
 
 interface UseDisclosureProps extends UseFloatingOptions {
-
+  default?: boolean,
 }
 
 export default function useDisclosure(props: UseDisclosureProps) {
+  const { 
+    default: defaultValue = false,
+  } = props
+
   const {refs, floatingStyles } = useFloating(props)
 
-  const hasFiredClickAway = useRef(false)
-  const [isPopperShown, setIsPopperShown] = useState(false)
-
+  const willTriggerAnchor = useRef(false)
+  const [isPopperShown, setIsPopperShown] = useState(defaultValue)
+  
   useClickAway(refs.floating, (event) => {
     if (event.target === refs.reference.current) {
-      hasFiredClickAway.current = true
+      willTriggerAnchor.current = true
     } else {
-      hasFiredClickAway.current = false
+      willTriggerAnchor.current = false
     }
     
     setIsPopperShown(false)   
   })
-
+  
   const open = useCallback(() => {
-    console.log("hasFiredClickAway.current", hasFiredClickAway.current)
-    if (hasFiredClickAway.current) {
-      hasFiredClickAway.current = false
+    if (willTriggerAnchor.current) {
+      willTriggerAnchor.current = false
       return
     }
     
@@ -43,8 +46,6 @@ export default function useDisclosure(props: UseDisclosureProps) {
       active: isPopperShown,
       setFloating: refs.setFloating,
       floatingStyles: floatingStyles
-    }),
-
-    onOpen: open
+    })
   }
 }
