74c6e2fddce4382030ea14b55873716c1d362431
Reyshawn <reshawnchang@gmail.com>
2023-11-05 00:07:25 +0800

    Remove the backup file after the restore;

--- scripts/parse-package.json.ts
+++ scripts/parse-package.json.ts
@@ -1,17 +1,18 @@
-import { readFile, writeFile, copyFile } from 'fs/promises';
+import { readFile, writeFile, copyFile, rm } from 'fs/promises';
 import * as path from 'path';
 
 const cwd = process.cwd()
 const sourcePath = path.join(cwd, "package.json")
-const retorePath = path.join(cwd, "package.json.backup")
+const restorePath = path.join(cwd, "package.json.backup")
 const command = process.argv[2]
 
 async function restore() {
-  await copyFile(retorePath, sourcePath)
+  await copyFile(restorePath, sourcePath)
+  await rm(restorePath)
 }
 
 async function prepare() {
-  await copyFile(sourcePath, retorePath)
+  await copyFile(sourcePath, restorePath)
   const packageJsonString = await readFile(sourcePath, "utf-8")
 
   const pkgObj = JSON.parse(packageJsonString)
