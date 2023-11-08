d45a6cb7220d91c4194a6f6cae88f46846849e5a
Reyshawn <reshawnchang@gmail.com>
2023-11-08 22:13:03 +0800

    Add a `build.ts` script to bundle the `@fete/components` package;

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,124 +1,123 @@
-import { rollup, RollupOptions, OutputOptions, InputPluginOption } from 'rollup';
+import { rollup, RollupOptions, OutputOptions, InputPluginOption, RollupBuild } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss'
+import { minify } from 'rollup-plugin-esbuild'
 import path from 'path';
+import { dts } from "rollup-plugin-dts";
 
-import { writeFile } from 'fs';
+import { rm } from 'fs/promises';
+import { existsSync } from 'fs';
+import { glob } from "glob"
+
+const cwd = process.cwd()
+const outputDir = path.join(cwd, "lib")
 
 
-const inputOptions: RollupOptions = {
+const mainOptions: RollupOptions = {
   input: 'src/index.ts',
   plugins: [
-    // resolve(),
-    // commonjs(),
     typescript({
-      jsx: "react-jsx"
+      jsx: "react-jsx",
+    }),
+    minify({
+      sourceMap: true
     }),
     postcss({
       extract: true,
-      minimize: true
+      minimize: true,
+      modules: {
+        generateScopedName: "fete_[local]_[hash:base64:7]",
+      }
     }) as InputPluginOption,
   ],
   external: ['react', 'react-dom', 'react/jsx-runtime']
 };
 
-// you can create multiple outputs from the same input to generate e.g.
-// different formats like CommonJS and ESM
+
+const dtsOptions: RollupOptions = {
+  input: 'src/index.ts',
+  plugins: [
+    dts()
+  ],
+  external: ['react', 'react-dom', 'react/jsx-runtime']
+};
+
+
 const outputOptionsList: OutputOptions[] = [{
   dir: "lib",
   format: "esm",
   preserveModules: true,
+  entryFileNames: "[name].mjs"
 }];
 
 
-
-build();
-
-async function build() {
-  let bundle;
-  let buildFailed = false;
+async function build(input: RollupOptions, output: OutputOptions[]) {
+  let bundle: RollupBuild | undefined
   try {
     // create a bundle
-    bundle = await rollup(inputOptions);
+    bundle = await rollup(input);
+    await generateOutputs(bundle, output);
 
-    // an array of file names this bundle depends on
-    console.log(bundle.watchFiles);
-
-    await generateOutputs(bundle);
-
-    // await bundle.write(outputOptionsList)
   } catch (error) {
-    buildFailed = true;
-    // do some error reporting
     console.error(error);
+  } finally {
+    bundle?.close()
   }
-  if (bundle) {
-    // closes the bundle
-    await bundle.close();
+}
+
+
+async function generateOutputs(bundle: RollupBuild, output: OutputOptions[]) {
+  for (const option of output) {
+    await bundle.write(option) 
   }
-  process.exit(buildFailed ? 1 : 0);
 }
 
 
-async function generateOutputs(bundle) {
-  for (const outputOptions of outputOptionsList) {
-    // generate output specific code in-memory
-    // you can call this function multiple times on the same bundle object
-    // replace bundle.generate with bundle.write to directly write to disk
-    //const { output } = await bundle.generate(outputOptions);
+function generateCSSBundleOptions(): [RollupOptions, OutputOptions[]][] {
+  return glob
+  .sync(path.join(cwd, 'src/**/*.module.css'))
+  .map(file => {
+    const source = path.relative("src", file)
+    return [
+      {
+        input: "src/" + source,
+        plugins: [
+          postcss({
+            extract: true,
+            minimize: true,
+            modules: {
+              generateScopedName: "fete_[local]_[hash:base64:7]",
+            }
+          }) as InputPluginOption,
+        ],
+      },
+      [
+        {
+          file: "lib/" + source.replace(/\.module(?=\.css$)/gm, ""),
+        }
+      ]
+    ]
+  })
+}
 
-    await bundle.write(outputOptions)
-    // for (const chunkOrAsset of output) {
-    //   if (chunkOrAsset.type === 'asset') {
-    //     // For assets, this contains
-    //     // {
-    //     //   fileName: string,              // the asset file name
-    //     //   source: string | Uint8Array    // the asset source
-    //     //   type: 'asset'                  // signifies that this is an asset
-    //     // }
-    //     console.log('Asset', chunkOrAsset);
-    //   } else {
-    //     // For chunks, this contains
-    //     // {
-    //     //   code: string,                  // the generated JS code
-    //     //   dynamicImports: string[],      // external modules imported dynamically by the chunk
-    //     //   exports: string[],             // exported variable names
-    //     //   facadeModuleId: string | null, // the id of a module that this chunk corresponds to
-    //     //   fileName: string,              // the chunk file name
-    //     //   implicitlyLoadedBefore: string[]; // entries that should only be loaded after this chunk
-    //     //   imports: string[],             // external modules imported statically by the chunk
-    //     //   importedBindings: {[imported: string]: string[]} // imported bindings per dependency
-    //     //   isDynamicEntry: boolean,       // is this chunk a dynamic entry point
-    //     //   isEntry: boolean,              // is this chunk a static entry point
-    //     //   isImplicitEntry: boolean,      // should this chunk only be loaded after other chunks
-    //     //   map: string | null,            // sourcemaps if present
-    //     //   modules: {                     // information about the modules in this chunk
-    //     //     [id: string]: {
-    //     //       renderedExports: string[]; // exported variable names that were included
-    //     //       removedExports: string[];  // exported variable names that were removed
-    //     //       renderedLength: number;    // the length of the remaining code in this module
-    //     //       originalLength: number;    // the original length of the code in this module
-    //     //       code: string | null;       // remaining code in this module
-    //     //     };
-    //     //   },
-    //     //   name: string                   // the name of this chunk as used in naming patterns
-    //     //   preliminaryFileName: string    // the preliminary file name of this chunk with hash placeholders
-    //     //   referencedFiles: string[]      // files referenced via import.meta.ROLLUP_FILE_URL_<id>
-    //     //   type: 'chunk',                 // signifies that this is a chunk
-    //     // }
-    //     console.log('Chunk', chunkOrAsset.fileName);
-// 
-// 
-    //    //  await writeFile(
-    //    //    path.resolve(__dirname, chunkOrAsset.fileName),
-    //    //    chunkOrAsset.module.code,
-    //    //    
-    //    //  )
-// 
-    //     // console.log('chunk::::', chunkOrAsset)
-    //   }
-    // }
+function buildCSS() {
+  const options = generateCSSBundleOptions()
+
+  for (const [input, output] of options) {
+    build(input, output)
   }
 }
+
+
+async function main() {
+
+  if (existsSync(outputDir)) {
+    await rm(outputDir, { recursive: true })
+  }
+  build(mainOptions, outputOptionsList)
+  build(dtsOptions, outputOptionsList)
+  buildCSS()
+}
+
+
+main()
